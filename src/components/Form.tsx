6d9e6e4e90c860ff10831b4382ccf55762649b60
Ana Luiza Rodrigues Chiamenti <analuizarodrigueschiamenti@gmail.com>
2023-07-31 16:10:47 -0300

    TESTE

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,90 +1,216 @@
-import React, { useState } from "react";
-import { useAppDispatch, useAppSelector } from "../store/hooks";
-import { useNavigate } from "react-router-dom";
+import React, { FormEvent, useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useAppSelector } from "../store/hooks";
+import { useAppDispatch } from "../store/hooks";
+import {
+  getNotesAsyncThunk,
+  loginAsyncThunk,
+  userCreateAsyncThunk,
+} from "../store/modules/UserLogged";
 
-interface FormProps {
-  mode: "login" | "create";
+interface FormCompProps {
+  textButton: "LOGAR" | "CADASTRAR";
+  mode: "login" | "signUp";
 }
 
-const Form: React.FC<FormProps> = ({ mode }) => {
+const FormComp: React.FC<FormCompProps> = ({ textButton, mode }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [repassword, setRepassword] = useState("");
+  const [errorEmail, setErrorEmail] = useState(false);
+  const [errorPassword, setErrorPassword] = useState(false);
+  const [errorRepassword, setErrorRepassword] = useState(false);
+  const [disabled, setDisabled] = useState(false);
+  const userlogged = useAppSelector((state) => state.userLogged.userLogged);
+  const listUsers = useAppSelector((state) => state.users.users);
+
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  const [alertSucess, setAlertSucess] = useState(false);
+  const [alertError, setAlertError] = useState(false);
+  const [alertErrorExist, setAlertErrorExist] = useState(false);
+
+  useEffect(() => {
+    if (mode === "signUp") {
+      const emailValid =
+        email.includes(".com") || (email.includes(".com.br") && email.includes("@"));
+
+      if (email.length > 0) {
+        setErrorEmail(!emailValid);
+      }
+
+      const passwordValid = password.length >= 6;
+      if (password.length > 0) {
+        setErrorPassword(!passwordValid);
+      }
+
+      const repasswordValid = password === repassword;
+      if (repassword.length > 0) {
+        setErrorRepassword(!repasswordValid);
+      }
+
+      setDisabled(!(emailValid && passwordValid && repasswordValid));
+    }
+  }, [email, password, repassword, mode]);
+
+  useEffect(() => {
+    if (userlogged.email) {
+      navigate("/notes");
+    }
+  }, [userlogged]);
+
+  function handleSubmit(evento: FormEvent) {
+    evento.preventDefault();
+
+    if (mode === "login") {
+      const user = {
+        email: email,
+        password: password,
+      };
+
+      const userExist = listUsers.find(
+        (value) => value.email === user.email && value.password === user.password,
+      );
+      if (!userExist) {
+        setAlertError(true);
+        setTimeout(() => {
+          setAlertError(false);
+        }, 5000);
+        return;
+      }
+
+      dispatch(loginAsyncThunk(user));
+      dispatch(getNotesAsyncThunk(email));
+    } else {
+      const newUser = {
+        email,
+        password,
+        repassword,
+      };
+
+      const retorno = listUsers.some((value) => value.email === newUser.email);
+      if (retorno) {
+        setAlertErrorExist(true);
+        setTimeout(() => {
+          setAlertErrorExist(false);
+        }, 5000);
+        return;
+      }
+
+      setAlertSucess(true);
+      setTimeout(() => {
+        setAlertSucess(false);
+      }, 5000);
+
+      dispatch(userCreateAsyncThunk({ email, password, repassword }));
+
+      setTimeout(() => {
+        navigate("/login");
+      }, 1000);
+    }
+  }
+
   return (
-    <React.Fragment>
-      <div className="mt-5 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form className="space-y-6" action="#" method="POST" onSubmit={(ev) => handleSubmit(ev)}>
+    <>
+      <div className="mt-4 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form className="space-y-2" onSubmit={(ev) => handleSubmit(ev)}>
           <div>
-            <label htmlFor="email" className="block text-sm leading-6 text-gray-900 font-mplus">
+            <label htmlFor="email" className="block text-sm font-medium leading-6 text-white">
               Email
             </label>
-            <div className="mt-2">
+
+            <div>
               <input
+                value={email}
+                onChange={(ev) => setEmail(ev.target.value)}
                 id="email"
                 name="email"
                 type="email"
                 autoComplete="email"
                 required
-                className="block w-full rounded-md border-0 py-1.5 text-pink-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-pink-400 focus:ring-2 focus:ring-inset focus:ring-pink-600 sm:text-sm sm:leading-6 focus: outline-none p-2"
+                className="block rounded-[22px] w-[100%] h-[10%] border-0 p-[4%] bg-orange-50 text-gray-900 shadow-sm focus:outline-none focus:bg-orange-100 "
               />
             </div>
           </div>
 
           <div>
-            <div className="flex items-center justify-between">
-              <label
-                htmlFor="password"
-                className="block text-sm leading-6 text-gray-900 font-mplus"
-              >
+            <div className="flex items-center justify-between mt-[5%]">
+              <label htmlFor="password" className="block text-sm font-medium leading-6 text-white">
                 Senha
               </label>
             </div>
-            <div className="mt-2">
+
+            <div>
               <input
+                value={password}
+                onChange={(ev) => setPassword(ev.target.value)}
                 id="password"
                 name="password"
                 type="password"
                 autoComplete="current-password"
                 required
-                className="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-pink-400 focus:ring-2 focus:ring-inset focus:ring-pink-600 sm:text-sm sm:leading-6 focus:border-0 focus: outline-none p-2"
+                className="block rounded-[22px] w-[100%] h-[10%] border-0 p-[4%] bg-orange-50 text-gray-900 shadow-sm focus:outline-none focus:bg-orange-100"
               />
             </div>
-            <div className="flex items-center justify-between mt-6">
-              <label
-                htmlFor="password"
-                className="block text-sm leading-6 text-gray-900 font-mplus"
-              >
-                Repetir a senha
-              </label>
-            </div>
-            <div className="mt-2">
-              <input
-                id="repassword"
-                name="repassword"
-                type="repassword"
-                autoComplete="current-password"
-                required
-                className="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-pink-400 focus:ring-2 focus:ring-inset focus:ring-pink-600 sm:text-sm sm:leading-6 focus:border-0 focus: outline-none p-2"
-              />
-            </div>
+
+            {mode === "signUp" && (
+              <>
+                <div className="flex items-center justify-between mt-[5%]">
+                  <label
+                    htmlFor="password"
+                    className="block text-sm font-medium leading-6 text-white"
+                  >
+                    Repetir a senha
+                  </label>
+                </div>
+
+                <div>
+                  <input
+                    value={repassword}
+                    onChange={(ev) => setRepassword(ev.target.value)}
+                    id="repassword"
+                    name="repassword"
+                    type="password"
+                    required
+                    className="block rounded-[22px] w-[100%] h-[10%] border-0 p-[4%] bg-orange-50 text-gray-900 shadow-sm focus:outline-none focus:bg-orange-100"
+                  />
+                </div>
+              </>
+            )}
           </div>
 
-          <div className="w-full flex justify-center pt-2">
+          <div className="w-full pt-5 flex justify-center">
             <button
               type="submit"
-              className="text-white w-80 h-10 bg-gradient-to-r from-pink-400 via-pink-500 to-pink-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-pink-300 dark:focus:ring-pink-800 rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 font-mplus"
+              disabled={disabled}
+              className="flex w-[85%] h-14 justify-center items-center rounded-[22px] bg-indigo-400 px-3 text-lg font-semibold leading-6 text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
-              Cadastrar
+              {textButton}
             </button>
           </div>
-        </form>
 
-        <p className="mt-7 text-center text-sm text-gray-500 font-mplus">
-          Ja tem uma conta?{" "}
-          <a href="/" className="font-mplus leading-6 text-pink-500 hover:text-[#ff9db0]">
-            Faça o login!
-          </a>
-        </p>
+          <div className="sm:mx-auto sm:w-full sm:max-w-sm flex justify-center">
+            {mode === "login" ? (
+              <a
+                href="/signup"
+                className="mt-2 text-center text-2xl leading-9 tracking-tight text-white text-[20px]"
+              >
+                Não tem uma conta? Cadastre-se!
+              </a>
+            ) : (
+              <a
+                href="/"
+                className="mt-2 text-center text-2xl leading-9 tracking-tight text-white text-[20px]"
+              >
+                Já tem uma conta? Entre agora mesmo!
+              </a>
+            )}
+          </div>
+        </form>
       </div>
-    </React.Fragment>
+    </>
   );
 };
 
-export default Form;
+export default FormComp;
